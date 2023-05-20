5eaec1239d6a8d5884721a44c02c1f06f5ce31aa
Rafael Silva <todosomtodavoz@gmail.com>
2023-05-20 11:53:08 -0300

    Substituindo resultado encontrado pela regex por outro texto - re.sub

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ dados_espanhol = pd.read_csv("base_dados/stackoverflow_espanhol.csv")
 questao_portugues = dados_portugues.Questão[5]
 # print(questao_portugues)
 
-retorno = re.findall(r"<.*?>", questao_portugues)
-print(retorno)
+# Retorna uma lista com todas as tags html no texto
+# retorno = re.findall(r"<.*?>", questao_portugues)
+# print(retorno)
+
+
+retorno_sub = re.sub(r"<.*?>", "  T-----E------S------T----E  ", questao_portugues)
+print(retorno_sub)
 
