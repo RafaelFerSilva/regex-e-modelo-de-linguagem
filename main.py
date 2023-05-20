98aaae5f2680127ac851c36abac76a13606b608c
Rafael Silva <todosomtodavoz@gmail.com>
2023-05-20 11:49:44 -0300

    Retornar todas as tags html dentro de texto utilizando regex re.findall(r'<.*?>', questao_portugues)

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import re
+
 dados_portugues =  pd.read_csv("base_dados/stackoverflow_portugues.csv")
 dados_ingles = pd.read_csv("base_dados/stackoverflow_ingles.csv")
 dados_espanhol = pd.read_csv("base_dados/stackoverflow_espanhol.csv")
-print(dados_espanhol.Questão[0])
+
+questao_portugues = dados_portugues.Questão[5]
+# print(questao_portugues)
+
+retorno = re.findall(r"<.*?>", questao_portugues)
+print(retorno)
+
