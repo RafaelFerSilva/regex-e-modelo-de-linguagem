7657f8eb0f271e934ab09d188386a52c8ae74811
Rafael Silva <todosomtodavoz@gmail.com>
2023-05-21 12:56:40 -0300

    Criar uma nova coluna somente com as questões sem code e sem html

--- main.py
+++ main.py
@@ -10,36 +10,8 @@ questao_portugues = dados_portugues.Questão[5]
 questao_espanhol = dados_espanhol.Questão[5]
 questao_ingles = dados_ingles.Questão[5]
 
-
-# print(questao_portugues)
-
-# Retorna uma lista com todas as tags html no texto
-# retorno = re.findall(r"<.*?>", questao_portugues)
-# print(retorno)
-
-# Substituir um texto por outro
-# retorno_sub = re.sub(r"<.*?>", "  T-----E------S------T----E  ", questao_portugues)
-# print(retorno_sub)
-
-
-# Transformar uma string em um regex e retornar onde o caracter está
-# retorno_search = re.search(r"70", "89745615749846541987945615749841568945679854170")
-# print(retorno_search)
-
-# Transformar uma string em um regex e retornar onde o caracter está -  Mais rápido
-# regex = re.compile(r"70")
-# retorno_compile = regex.search("89745615749846541987945615749841568945679854170")
-# print(retorno_compile)
-
-# Comparando os métodos
-# setup = """import re"""
-# tempo = timeit("""re.search(r"70", "89745615749846541987945615749841568945679854170")""", setup)
-# print(tempo)
-#
-# setup = """import re
-# regex = re.compile(r"70")"""
-# tempo = timeit("""regex.search("89745615749846541987945615749841568945679854170")""", setup)
-# print(tempo)
+regex_html = re.compile(r"<.*?>")
+regex_code = re.compile(r"<code>(.|(\n))*?</code>")
 
 def remover(textos, regex):
     if type(textos) == str:
@@ -55,9 +27,21 @@ def substituir_codigo(textos, regex):
         return [regex.sub("CODE", texto) for texto in textos]
 
 
-# questao_sem_tag = remover(questao_ingles, regex_html)
-# print(questao_sem_tag)
+questoes_port_sem_codigo = substituir_codigo(dados_portugues.Questão, regex_code)
+questoes_port_sem_codigo_sem_tag = remover(questoes_port_sem_codigo, regex_html)
 
-regex_code = re.compile(r"<code>(.|(\n))*?</code>")
-questao_sem_code = substituir_codigo(questao_portugues, regex_code)
-print(questao_sem_code)
+questoes_esp_sem_codigo = substituir_codigo(dados_espanhol.Questão, regex_code)
+questoes_esp_sem_codigo_sem_tag = remover(questoes_esp_sem_codigo, regex_html)
+
+questoes_ing_sem_codigo = substituir_codigo(dados_ingles.Questão, regex_code)
+questoes_ing_sem_codigo_sem_tag = remover(questoes_ing_sem_codigo, regex_html)
+
+dados_portugues["sem_code_sem_tag"] = questoes_port_sem_codigo_sem_tag
+dados_espanhol["sem_code_sem_tag"] = questoes_esp_sem_codigo_sem_tag
+dados_ingles["sem_code_sem_tag"] = questoes_ing_sem_codigo_sem_tag
+
+
+print(dados_espanhol.sem_code_sem_tag[0])
+
+
+
